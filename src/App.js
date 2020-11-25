644c331d28e1e85d3a1c65c0a8b283699681eb99
Gundalf72 <andrew.lowcock@gmail.com>
2020-11-25 12:51:21 +0000

    back to where we started

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to barf.
+          Edit <code>src/App.js</code> and save to xxx.
         </p>
         <a
           className="App-link"
